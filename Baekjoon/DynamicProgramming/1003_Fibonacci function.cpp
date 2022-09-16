838314cda0fdbcccb25d7df3d3c38f2437bfed78
DOUBLEMONS <monmon867867@gmail.com>
2022-09-16 19:50:20 +0900

    문제 추가

---
+++ Baekjoon/DynamicProgramming/1003_Fibonacci function.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+int dp[40];
+
+int fibonacci(int T) {
+    if (T == 0) {
+        printf("0");
+        return 0;
+    }
+    else if (T == 1) {
+        printf("1");
+        return 1;
+    }
+    else {
+        return fibonacci(T - 1) + fibonacci(T - 2);
+    }
+}
+
+int main() {
+
+	int T, N = 0;
+	cin >> T;
+    dp[0] = 1; // 0
+    dp[1] = 2; // 0 1
+
+    for (int i = 1; i <= T; i++)
+    for (int i = 1; i <= N; T++) {
+        fibonacci(N);
+        
+    }
+    cout << (dp[N] == 0 ? 0 : dp[N]) << endl;
+    return 0;
+}
+
+// 0 = 0
+// 1 = 1
+// 0 1 = 1
+// 1 0 1 = 2
+// 1 0 1 0 1 = 3
