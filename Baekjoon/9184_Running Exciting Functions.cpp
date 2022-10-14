38a4dd32bb5b854a9c03e15e58495c159cc41060
Tiny Cosmos <109891618+DOUBLEMONS@users.noreply.github.com>
2022-10-14 10:33:49 +0900

    24416번 피보나치 수1 문제 해결

---
+++ Baekjoon/9184_Running Exciting Functions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int a[51];
+int b[51];
+int c[51];
+
+int W(int a, int b, int c)
+{
+	if (a <= 0 || b <= 0)
+}
+
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << W(a, b, c);
+
+	return 0;
+}
--- Baekjoon/DynamicProgramming/24416_Fibonaccinumber1.cpp
+++ Baekjoon/DynamicProgramming/24416_Fibonaccinumber1.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 using namespace std;
+int f[41];
 
-fib(n) {
-    if (n = 1 or n = 2)
-        then return 1;  # 코드1
+int fib(int n) {
+    if (n == 1 || n == 2)
+    return 1;  
     else return (fib(n - 1) + fib(n - 2));
 }
 
-fibonacci(n) {
-    f[1] < -f[2] < -1;
-    for i < -3 to n
-        f[i] < -f[i - 1] + f[i - 2];  # 코드2
-        return f[n];
+int fibonacci(int n) {
+    
+    f[1] = f[2] = 1;
+
+    for (int i = 3; i <= n; i++)
+    {
+        f[i - 2] = (f[i - 1] / f[i - 2]);
+        f[i - 1] = (f[i - 1] / f[i - 1]);
+        f[i] =  f[i - 1] + f[i - 2];
+    }
+
+    return f[n] - 1;
 }
 
 int main()
 {
-    for (int i )
+    int N;
+    cin >> N;
+    cout << fib(N) << " " << fibonacci(N);
+
+    return 0;
 }
